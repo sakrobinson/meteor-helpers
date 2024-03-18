7872d7b04de91847a67b00fa809d17528c25c35d
sakrobinson <scott.austen.robinson@gmail.com>
2024-03-17 21:18:05 -0600

    updated cassandra node

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from get_forecast import GetWeatherForecast
 import uuid
 
 # Connect to Cassandra
-cluster = Cluster(['cassandra-node1', 'cassandra-node2'])
+cluster = Cluster(['38.242.246.51']) # Cassandra Node 1 Server
 session = cluster.connect('weather_data')
 
 # Fetch data

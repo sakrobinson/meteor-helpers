d5f639753f4619014e851be6cea5e75be1c0fce6
sakrobinson <scott.austen.robinson@gmail.com>
2024-03-26 21:19:31 -0600

    add actuals pull

---
+++ insert_actuals.py
@@ -0,0 +1,53 @@
+from cassandra.cluster import Cluster
+from forecast import GetWeatherHistory
+from datetime import datetime
+import uuid
+
+# days back
+actual_days = 10
+
+# Connect to Cassandra
+cluster = Cluster(['172.17.0.2'])  # Adjust IP as needed
+session = cluster.connect('weather_data')
+
+# Fetch data
+file_path = 'worldcities_sample.csv'
+w_actual = GetWeatherHistory(file_path)
+
+# Calculate dates for the last 10 days
+start_date, end_date = w_actual.calculate_history_dates(days_back=10)
+
+# Fetch historical data for the last n days
+actuals = w_actual.get_actuals(days_back=actual_days)
+
+# Generate a unique run_id
+date_prefix = datetime.now().strftime("%Y%m%d")
+run_id = f"{date_prefix}_{uuid.uuid4().int}"
+
+# Prepare the insert statement
+insert_actual = session.prepare("""
+    INSERT INTO actual_weather (run_id, id, city_ascii, country, date, temperature_2m, pressure_msl, windspeed_10m, relativehumidity_2m)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+""")
+
+# Insert data into Cassandra
+for actual in actuals:
+    hourly_data = actual['hourly']
+    times = hourly_data['time']
+    
+    for time_point in times:
+        index = times.index(time_point)
+        session.execute(insert_actual, (
+            run_id,
+            str(actual['id']),
+            actual['city_ascii'],
+            actual['country'],
+            datetime.strptime(time_point, "%Y-%m-%dT%H:%M"),
+            hourly_data['temperature_2m'][index],
+            hourly_data['pressure_msl'][index],
+            hourly_data['windspeed_10m'][index],
+            hourly_data['relativehumidity_2m'][index]
+        ))
+
+# Close the connection
+cluster.shutdown()
--- test_fcast_actual.py
+++ test_fcast_actual.py
@@ -9,6 +9,10 @@ cities_df = pd.read_csv(file_path)
 first_city = cities_df.iloc[0]
 latitude = first_city['lat']
 longitude = first_city['lng']
+id = first_city['id']
+name = first_city['city_ascii'] 
+country = first_city['country']
+
 
 # Instantiate the classes
 forecast_instance = GetWeatherForecast(file_path)
@@ -18,9 +22,10 @@ history_instance = GetWeatherHistory(file_path)
 forecast_start_date, forecast_end_date = forecast_instance.calculate_forecast_dates(days_ahead=1)
 actuals_start_date, actuals_end_date = history_instance.calculate_history_dates(days_back=1)
 
+
 # Get forecast and actual weather data for the first city
-forecast_data = forecast_instance.get_weather_data(latitude, longitude, forecast_start_date, forecast_end_date)
-actuals_data = history_instance.get_weather_data(latitude, longitude, actuals_start_date, actuals_end_date)
+forecast_data = forecast_instance.get_weather_data(latitude, longitude, forecast_start_date, forecast_end_date, id, name, country)
+actuals_data = history_instance.get_weather_data(latitude, longitude, actuals_start_date, actuals_end_date, id, name, country)
 
 # Print the results
 print("Forecast Data for the first city:", forecast_data)

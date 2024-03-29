ea17daac069fca7275733e9d5e8e9df0d2102ce0
sakrobinson <scott.austen.robinson@gmail.com>
2024-03-29 11:40:29 -0600

    update config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@
      apps : [{
        name: 'meteor_chron',
        script: '/usr/bin/python3',
-       args: '~/meteor-helpers/run.py',
+       args: 'run.py',
        instances: 1,
        autorestart: false,
        watch: false,
        max_memory_restart: '1G',
-       cron_restart: '0 7 * * *', // This will restart at 7:00 AM every day
+       cron_restart: '0 1 * * *', // This will restart at 1:00 PM every day
        out_file: '/path/to/logfile.log', // Need to add logging
        error_file: '/path/to/errorfile.log', // Need to add logging
        merge_logs: true,

ba252e6b54b0ddb4228222142e4bc194a542692a
sakrobinson <70336185+sakrobinson@users.noreply.github.com>
2024-06-14 15:51:48 -0600

    Update ecosystem.config.js
    updated hour call

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
-   module.exports = {
-     apps : [{
-       name: 'meteor_chron',
-       script: '/usr/bin/python3',
-       args: 'run.py',
-       instances: 1,
-       autorestart: false,
-       watch: false,
-       max_memory_restart: '1G',
-       cron_restart: '0 1 * * *', // This will restart at 1:00 PM every day
-       out_file: '/path/to/logfile.log', // Need to add logging
-       error_file: '/path/to/errorfile.log', // Need to add logging
-       merge_logs: true,
-       env: {
-        TZ: "America/Denver"
-       }
-     }]
-   };
+module.exports = {
+  apps: [{
+    name: 'meteor_chron',
+    script: '/usr/bin/python3',
+    args: 'run.py',
+    instances: 1,
+    autorestart: false,
+    watch: false,
+    max_memory_restart: '1G',
+    cron_restart: '0 16 * * *', // This will restart at 4:00 PM every day (using 24-hour format)
+    out_file: '/path/to/logfile.log', // Path to log file
+    error_file: '/path/to/errorfile.log', // Path to error log file
+    merge_logs: true,
+    env: {
+      TZ: 'America/Denver'
+    }
+  }]
+};
